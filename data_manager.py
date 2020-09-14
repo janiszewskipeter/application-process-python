724441acff18901dbfd9611193af3570c653503c
Piotr Janiszewski <janiszewskipeter@gmail.com>
2020-09-14 19:51:59 +0200

    Final version, all needed features working

--- data_manager.py
+++ data_manager.py
@@ -29,7 +29,7 @@ def get_mentors_by_last_name(cursor: RealDictCursor, last_name: str) -> list:
 
 @database_common.connection_handler
 def get_mentors_by_city(cursor: RealDictCursor, mentor_city: str) -> list:
-    query ="""
+    query = """
         SELECT first_name, last_name, city
         FROM mentor
         WHERE city = %(mentor_city)s
@@ -39,3 +39,110 @@ def get_mentors_by_city(cursor: RealDictCursor, mentor_city: str) -> list:
     return cursor.fetchall()
 
 
+
+@database_common.connection_handler
+def get_all_applicans(cursor: RealDictCursor) -> list:
+    query ="""
+        SELECT first_name, last_name, phone_number, application_code
+        FROM applicant
+        ORDER BY first_name"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+@database_common.connection_handler
+def get_applicant_data_by_name(cursor: RealDictCursor, applicant_name: str) -> list:
+    query ="""
+        SELECT first_name, last_name, phone_number FROM applicant
+        WHERE first_name = %(applicant_name)s OR last_name = %(applicant_name)s
+        ORDER BY first_name"""
+    args = {'applicant_name': applicant_name}
+    cursor.execute(query, args)
+    return cursor.fetchall()
+
+
+# @database_common.connection_handler
+# def get_applicant_data_by_email(cursor: RealDictCursor, applicants_email: str) -> list:
+#     query = """
+#             SELECT * FROM applicant
+#             WHERE email LIKE %(applicants_email)s
+#             ORDER BY first_name"""
+#     args = {'applicants_email': applicants_email}
+#     cursor.execute(query, args)
+#     return cursor.fetchall()
+
+@database_common.connection_handler
+def get_applicants_data_by_email(cursor: RealDictCursor, applicans_email: str) -> list:
+    cursor.execute("""
+        SELECT first_name, last_name, phone_number
+        FROM applicant
+        WHERE email LIKE (%s)
+        ORDER BY first_name""", ["%"+applicans_email])
+    return cursor.fetchall()
+
+
+# SELECT first_name, last_name, phone_number FROM applicant
+#             WHERE email LIKE %(applicants_email)s
+#             ORDER BY first_name"""
+
+@database_common.connection_handler
+def get_applicant_info(cursor: RealDictCursor, application_code: str) -> list:
+    query = """
+                        SELECT * FROM applicant
+                        WHERE application_code = %(application_code)s
+                        ORDER BY first_name
+                       """
+    args={'application_code': application_code}
+    cursor.execute(query, args)
+
+    return cursor.fetchall()
+
+@database_common.connection_handler
+def update_nr(cursor: RealDictCursor, new_nr: str, application_code: str) -> list:
+    query = """
+                            UPDATE applicant
+                            SET phone_number= %(new_nr)s
+                            WHERE application_code = %(application_code)s
+                           """
+    args = {'application_code': application_code, 'new_nr': new_nr}
+    cursor.execute(query, args)
+
+    return None
+
+
+@database_common.connection_handler
+def delete(cursor: RealDictCursor, application_code: str) -> list:
+    query = """
+                            DELETE FROM applicant
+                            WHERE application_code = %(application_code)s
+                           """
+    args = {'application_code': application_code}
+    cursor.execute(query, args)
+
+    return None
+
+
+@database_common.connection_handler
+def delete_by_email_ending(cursor: RealDictCursor, applicans_email: str) -> list:
+        cursor.execute("""
+            DELETE FROM applicant
+            WHERE email LIKE (%s)
+            """,
+                       ["%" + applicans_email])
+        return None
+
+
+@database_common.connection_handler
+def append_applicant(cursor: RealDictCursor, name: str,phone: int, last_name: str, email: str,code: int) -> list:
+    query = """
+                                INSERT INTO applicant (first_name, last_name, phone_number, email, application_code)
+                                VALUES (%(name)s, %(last_name)s, %(phone)s ,%(email)s, %(code)s);
+                               """
+    args = {'name': name,
+            'last_name' : last_name,
+            'phone' : phone,
+            'email': email,
+            'code' : code
+
+            }
+
+    cursor.execute(query, args)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
-
+import random
 import data_manager
 
 app = Flask(__name__)
@@ -7,17 +7,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+
     return render_template('index.html')
 
 
 @app.route('/mentors')
 def mentors_list():
     mentor_name = request.args.get('mentor-last-name')
-    mentor_city = request.args.get('city-name')
-    if mentor_city:
-        mentor_details = data_manager.get_mentors_by_city(mentor_city)
-    else:
-        mentor_details = data_manager.get_mentors()
     if mentor_name:
         mentor_details = data_manager.get_mentors_by_last_name(mentor_name)
     else:
@@ -34,5 +30,84 @@ def mentors_by_city():
     else:
         mentor_details = data_manager.get_mentors()
     return render_template('mentors.html', mentors=mentor_details)
+
+@app.route('/applicants')
+def applicants():
+    applicant_name = request.args.get('applicant_name')
+
+    if applicant_name:
+        details = data_manager.get_applicant_data_by_name(applicant_name)
+    else:
+        details = data_manager.get_all_applicans()
+    return render_template('applicants.html', applicans=details)
+
+@app.route('/applicants-phone')
+def applicants_phone():
+    applicant_email = request.args.get('applicant_email')
+    if applicant_email:
+        details = data_manager.get_applicants_data_by_email(applicant_email)
+    else:
+        details = data_manager.get_all_applicans()
+    return render_template('applicants-phone.html', applicans=details)
+
+@app.route('/applicant-info/<application_code>')
+def applicant_info(application_code):
+    applicant_info = data_manager.get_applicant_info(application_code)
+    return render_template('applicants-info.html', applicant_info=applicant_info)
+
+
+@app.route('/update_nr/<application_code>', methods=['POST'])
+def update_number_in_db(application_code):
+    if request.method == "POST":
+
+        new_nr = request.form['new_nr']
+        # new_nr = request.args.get('new_nr')
+        # print(text)
+        # print(new_nr)
+        data_manager.update_nr(new_nr, application_code)
+        applicant_info = data_manager.get_applicant_info(application_code)
+        return render_template('applicants-info.html', applicant_info=applicant_info)
+    else:
+        applicant_info = data_manager.get_applicant_info(application_code)
+        return render_template('applicants-info.html', applicant_info=applicant_info)
+
+
+@app.route('/delete/<application_code>', methods=['POST'])
+def delete_user(application_code):
+    if request.method == "POST":
+        data_manager.delete(application_code)
+        details = data_manager.get_all_applicans()
+        return render_template('applicants.html', applicans=details)
+    else:
+        applicant_info = data_manager.get_applicant_info(application_code)
+        return render_template('applicants-info.html', applicant_info=applicant_info)
+
+@app.route('/delete_by_email_ending', methods=['POST'])
+def delete_by_email_ending() :
+    if request.method == "POST":
+        email_end = request.form['email-ending']
+        data_manager.delete_by_email_ending(email_end)
+        details = data_manager.get_all_applicans()
+        return render_template('applicants.html', applicans=details)
+    return None
+
+@app.route('/new')
+def new():
+    return render_template('add_applicant.html')
+
+@app.route('/add_applicant', methods=['POST'])
+def add_applicant():
+    if request.method == 'POST':
+        name = request.form['name']
+        last_name = request.form['last_name']
+        phone = request.form['phone_nr']
+        email= request.form['email']
+        code = random.randint(100, 1000)
+        print(code)
+        data_manager.append_applicant(name, last_name, phone, email,code)
+        applicant_info = data_manager.get_applicant_info(code)
+        return render_template('applicants-info.html', applicant_info=applicant_info)
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
